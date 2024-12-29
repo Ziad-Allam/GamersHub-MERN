cb52b08093252eadb214c4803af6e1e28ff2fca0
Ziad Allam <ziadmahmoudallam@gmail.com>
2024-12-30 01:56:07 +0200

    Add loading functionality to bannar

--- frontend/src/components/home/Banner.jsx
+++ frontend/src/components/home/Banner.jsx
@@ -8,37 +8,49 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import { Navigation, Autoplay } from 'swiper/modules';
+import Loading from '../loading/Loading';
 
 function Banner() {
 
   const dispatch = useDispatch()
 
-  const images = useSelector((state) => state.banner.bannarImage.images)
+  const images = useSelector((state) => state.banner.bannarImage.images);
+  const { isLoading } = useSelector((state) => state.banner);
 
   useEffect(() => {
     dispatch(getBannerImages())
   }, [])
 
   return (
-    <Swiper
-      navigation={true}
-      modules={[Navigation, Autoplay]}
-      autoplay={{
-        delay: 3000, // Time between slides (in milliseconds)
-        disableOnInteraction: false, // Keeps autoplay active even after manual interaction
-      }}
-      preventClicksPropagation={false}
-      className="mySwiper">
+    <>
+      {isLoading ?
+        <Loading type='bannarLoading' />
+        :
 
-      {
-        images && images.length > 0 ?
-          images.map(bannarImage => (
-            <SwiperSlide key={bannarImage?._id}>
-              <img src={bannarImage.image.url} alt="" className='w-full h-64 sm:h-80 md:h-full object-cover' />
-            </SwiperSlide>
-          )) : null
+        <Swiper
+          navigation={true}
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 3000, // Time between slides (in milliseconds)
+            disableOnInteraction: false, // Keeps autoplay active even after manual interaction
+          }}
+          preventClicksPropagation={false}
+          className="mySwiper">
+
+          {
+            images && images.length > 0 ?
+              images.map(bannarImage => (
+                <SwiperSlide key={bannarImage?._id}>
+                  <img src={bannarImage.image.url} alt="" className='w-full h-64 sm:h-80 md:h-full object-cover' />
+                </SwiperSlide>
+              )) : <div className='w-full h-64 sm:h-80 md:h-[438px] object-cover bg-gray-200'>Image not available</div>
+
+          }
+        </Swiper>
+
       }
-    </Swiper>
+
+    </>
   )
 }
 
