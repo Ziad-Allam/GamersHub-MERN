2bb04e36c494bfe132ca4a7ab4f8dbb3c4485482
Ziad Allam <ziadmahmoudallam@gmail.com>
2024-12-30 02:18:58 +0200

    Add loading in productListByCategory component

--- frontend/src/pages/products/ProductsListByCategory.jsx
+++ frontend/src/pages/products/ProductsListByCategory.jsx
@@ -4,28 +4,31 @@ import { useParams } from 'react-router-dom'
 import { fetchProductsByCategory } from '../../features/products/productsSlice'
 import ProductCard from '../../components/products/ProductCard'
 import ProductListingHeader from '../../components/products/ProductListingHeader'
+import Loading from '../../components/loading/Loading'
 
 function ProductsListByCategory() {
 
   const { categoryName, id } = useParams()
   const dispatch = useDispatch()
   // const { productList } = useSelector((state) => state.products)
-  const { productList } = useSelector((state) => state.products)
+  const { productList,isLoading } = useSelector((state) => state.products)
   const sort = useSelector((state) => state.sort.sort); // Access sort state from Redux
 
   useEffect(() => {
     dispatch(fetchProductsByCategory({ id, sortBy: sort }))
-  }, [id,sort])
+  }, [id, sort])
   return (
     <div className='py-6'>
-      {/* <h1 className='font-medium capitalize text-2xl'>{productList.categoryName}</h1> */}
       <ProductListingHeader title={productList.categoryName} filteredProducts={productList.productsCategory} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-4 ">
         {
-          productList.productsCategory?.map(productList =>
-            <ProductCard key={productList._id} product={productList} />
-          )
+          isLoading ?
+            <Loading repeat={15} type="productCard" />
+            :
+            productList.productsCategory?.map(productList =>
+              <ProductCard key={productList._id} product={productList} />
+            )
         }
       </div>
 
