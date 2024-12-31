2bb6557d8733651abd3ccd43664d35bf74a1569f
Ziad Allam <ziadmahmoudallam@gmail.com>
2024-12-31 02:53:37 +0200

    Update categories component

--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -6,11 +6,11 @@ function Categories({ categoryList }) {
     return (
         <div className='py-8'>
             <Swiper
-                slidesPerView={2.5}
+                slidesPerView={3.5}
                 spaceBetween={5}
                 className="mySwiper"
                 breakpoints={{
-                    500: { slidesPerView: 3.5, spaceBetween: 5 },
+                    500: { slidesPerView: 4.5, spaceBetween: 5 },
                     800: { slidesPerView: 5.5, spaceBetween: 5 },
                     1024: { slidesPerView: 7.5, spaceBetween: 5 },
                     1290: { slidesPerView: 10, spaceBetween: 5 },
@@ -21,10 +21,10 @@ function Categories({ categoryList }) {
                         <SwiperSlide key={category?._id}>
                             <Link to={`/shop/categories/${category.slug}/${category._id}`}>
                                 <div className='flex flex-col items-center gap-2 '>
-                                    <div className='w-20 h-20 sm:w-24 sm:h-24 relative'>
+                                    <div className='w-16 h-16 sm:w-24 sm:h-24 relative'>
                                         <img src={category?.image.url} alt="" className=' object-cover' />
                                     </div>
-                                    <p className='capitalize text-sm sm:text-base font-semibold'>{category?.title}</p>
+                                    <p className='capitalize text-xs sm:text-base font-semibold'>{category?.title}</p>
                                 </div>
                             </Link>
                         </SwiperSlide>
