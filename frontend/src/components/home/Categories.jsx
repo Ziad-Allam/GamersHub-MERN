ef68f2694992d88dfd894e97b2385c7ad8a9d6e3
Ziad Allam <ziadmahmoudallam@gmail.com>
2025-01-01 01:26:45 +0200

    Handle loading in categories

--- frontend/src/components/home/Categories.jsx
+++ frontend/src/components/home/Categories.jsx
@@ -1,36 +1,45 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Link } from 'react-router-dom';
+import Loading from '../loading/Loading';
+import { useSelector } from 'react-redux';
 
-function Categories({ categoryList }) {
+function Categories() {
+
+    const { categoryList, isLoading } = useSelector((state) => state.categories)
+
     return (
         <div className='py-8'>
-            <Swiper
-                slidesPerView={3.5}
-                spaceBetween={5}
-                className="mySwiper"
-                breakpoints={{
-                    500: { slidesPerView: 4.5, spaceBetween: 5 },
-                    800: { slidesPerView: 5.5, spaceBetween: 5 },
-                    1024: { slidesPerView: 7.5, spaceBetween: 5 },
-                    1290: { slidesPerView: 10, spaceBetween: 5 },
-                }}>
+            {isLoading ?
+                <Loading type='categories' />
+                :
+                <Swiper
+                    slidesPerView={3.5}
+                    spaceBetween={5}
+                    className="mySwiper"
+                    breakpoints={{
+                        500: { slidesPerView: 4.5, spaceBetween: 5 },
+                        800: { slidesPerView: 5.5, spaceBetween: 5 },
+                        1024: { slidesPerView: 7.5, spaceBetween: 5 },
+                        1290: { slidesPerView: 10, spaceBetween: 5 },
+                    }}>
 
-                {categoryList?.map((category) => {
-                    return (
-                        <SwiperSlide key={category?._id}>
-                            <Link to={`/shop/categories/${category.slug}/${category._id}`}>
-                                <div className='flex flex-col items-center gap-2 '>
-                                    <div className='w-16 h-16 sm:w-24 sm:h-24 relative'>
-                                        <img src={category?.image.url} alt="" className=' object-cover' />
+                    {categoryList?.map((category) => {
+                        return (
+                            <SwiperSlide key={category?._id}>
+                                <Link to={`/shop/categories/${category.slug}/${category._id}`}>
+                                    <div className='flex flex-col items-center gap-2 '>
+                                        <div className='w-16 h-16 sm:w-24 sm:h-24 relative'>
+                                            <img src={category?.image.url} alt="" className=' object-cover' />
+                                        </div>
+                                        <p className='capitalize text-xs sm:text-base font-semibold'>{category?.title}</p>
                                     </div>
-                                    <p className='capitalize text-xs sm:text-base font-semibold'>{category?.title}</p>
-                                </div>
-                            </Link>
-                        </SwiperSlide>
-                    )
-                })}
-            </Swiper>
+                                </Link>
+                            </SwiperSlide>
+                        )
+                    })}
+                </Swiper>
+            }
 
         </div>
     )
